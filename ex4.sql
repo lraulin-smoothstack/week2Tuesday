ed4e2a5a57668cbee1272d491172fc8a81a1d6dc
Lee Raulin <leeraulin@gmail.com>
2019-12-18 16:28:40 -0500

    ...

--- ex4.sql
+++ ex4.sql
@@ -1,3 +1,5 @@
+-- For each book that is loaned out from the "Sharpstown" branch and whose DueDateis today, retrieve the book title, the borrower's name, and the borrower's address
+
 SELECT
     tbl_book.title,
     tbl_borrower.name,
@@ -5,19 +7,19 @@ SELECT
 FROM
     tbl_book_loans
     JOIN
-        tbl_book
-        ON tbl_book.bookId = tbl_book_loans.bookId
+    tbl_book
+    ON tbl_book.bookId = tbl_book_loans.bookId
     JOIN
-        tbl_borrower
-        ON tbl_borrower.cardNo = tbl_book_loans.cardNo
+    tbl_borrower
+    ON tbl_borrower.cardNo = tbl_book_loans.cardNo
 WHERE
     branchId IN
     (
         SELECT
-            branchId
-        FROM
-            tbl_library_branch
-        WHERE
+        branchId
+    FROM
+        tbl_library_branch
+    WHERE
             branchName = "Sharptown"
     )
     AND dueDate = CURDATE() ;
